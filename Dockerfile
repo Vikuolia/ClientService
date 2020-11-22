1f8e3a74e68025852b9363a8b7d948267a2c7642
Viktoriia Ivanova <iv.vik.19.07@gmail.com>
2020-11-22 19:49:35 +0200

    change dockerfile, config file and connection to db

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM openjdk:11-jre
-WORKDIR D: /3 курс/ТРСПО/lab3/client/out/artifacts/client_jar
-EXPOSE 8090
+FROM openjdk:14.0.2-jdk
+COPY ./target/*.jar /app.jar
+EXPOSE 8087
 ENTRYPOINT ["java","-jar","/app.jar"]
--- src/main/java/com/example/client/api/ClientController.java
+++ src/main/java/com/example/client/api/ClientController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/client")
+@RequestMapping("/")
 public class ClientController {
 
     private final ClientService clientService;
